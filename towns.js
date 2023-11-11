b1eb6dab26b748fd7769ddb3ff931c6812726eb2
bgatanasov <viktorybg@outlook.com>
2023-11-11 16:49:34 +0200

    add new functio botton add

--- towns.js
+++ towns.js
@@ -19,3 +19,9 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 }
+function addTown() {
+	let townName = $('#townNameForAdd').val();
+	$('#townNameForAdd').val('');
+	$('#towns').append($('<option>').text(townName));
+	$('#result').text(townName + " added.");
+}
